cc7a4efc6ca7c5c59f17fe896bc25c03e06db854
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-18 21:43:28 +0530

    (feat): spotify search

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Form } from "react-bootstrap";
 import useAuth from "./useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
@@ -8,8 +8,49 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 const Dashboard: React.FC<{ code: string }> = ({ code }) => {
-  const accessToken = useAuth(code);
+  const accessToken: string = useAuth(code) || "";
   const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState<
+    Array<{ artist: string; title: string; uri: string; albumUrl: string }>
+  >([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      if (!res.body.tracks) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if ((image.height ?? 1) < (smallest.height ?? 0)) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+  }, [search, accessToken]);
+
   return (
     <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
       <Form.Control
@@ -19,7 +60,7 @@ const Dashboard: React.FC<{ code: string }> = ({ code }) => {
         onChange={(e) => setSearch(e.target.value)}
       ></Form.Control>
       <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-        Songs
+        {searchResults[0] && searchResults[0].title}
       </div>
       <div className="">Bottom</div>
     </Container>
