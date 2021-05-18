3a771365df0b059705ea149b9409f400b28f43a9
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-18 13:52:35 +0530

    (feat): add search'

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
 import useAuth from "./useAuth";
 
 const Dashboard: React.FC<{ code: string }> = ({ code }) => {
   const accessToken = useAuth(code);
-  return <div>{accessToken}</div>;
+  const [search, setSearch] = useState("");
+  return (
+    <Container>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      ></Form.Control>
+    </Container>
+  );
 };
 
 export default Dashboard;
